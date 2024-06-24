2a1edba8d4eaebee0348ee6b18d5b05474299e84
Leenard <lolleenard0@gmail.com>
2024-06-24 22:01:36 +0800

    feat: traversal

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,36 @@
+const traverseNode = ({ node, parent, visitor }) => {
+  const methods = visitor[node.type];
+
+  if (methods && methods.enter) {
+    methods.enter({ node, parent });
+  }
+
+  if (node.arguments) {
+    traverseArray({ node: node.arguments, parent: node, visitor });
+  }
+
+  if (methods && methods.exit) {
+    methods.exit({ node, parent });
+  }
+};
+
+const traverseArray = ({ array, parent, visitor }) => {
+  array.forEach((node) => {
+    traverseNode({
+      node,
+      parent,
+      visitor,
+    });
+  });
+};
+
 const traverse = (node, visitor) => {};
 
 module.exports = { traverse };
+
+// const visitor = {
+//   VariableDeclaration({ node, paren }) {
+//     enter({ node, parent }) {},
+//     exit() {}
+//   },
+// };
