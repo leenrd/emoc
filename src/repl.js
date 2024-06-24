1e2fe37a674ef0eb65a3efe493c4ba47a69cf591
Leenard <lolleenard0@gmail.com>
2024-06-24 15:13:51 +0800

    feat: lib standard

--- src/repl.js
+++ src/repl.js
@@ -3,6 +3,14 @@ const chalk = require('chalk');
 
 const { parseAndEvaluate } = require('./parse-and-evaluate');
 
+const askQuestions = () => {
+  const questions = [
+    { name: 'COMMAND', type: 'input', message: chalk.blue('>') },
+  ];
+
+  return prompt(questions);
+};
+
 const repl = async () => {};
 
 if (require.main === module) {
